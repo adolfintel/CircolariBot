e734516c81791c950e1349a8f5e153ded216bd70
Federico Dossena <info@fdossena.com>
2025-08-05 16:09:06 +0200

    Aggiornato per il nuovo sito

--- src/main/java/com/dosse/circolaribot/Main.java
+++ src/main/java/com/dosse/circolaribot/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2024 Federico Dossena
+ * Copyright (C) 2021-2025 Federico Dossena
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@ package com.dosse.circolaribot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -28,7 +29,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.security.MessageDigest;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashMap;
 import org.ini4j.Wini;
@@ -43,24 +43,26 @@ import org.jsoup.select.Elements;
  */
 public class Main {
 
-    private static final String URL = "https://www.galileicrema.edu.it/arc_circolari?items_per_page=All&field_anno_scolastico_value=$$$ANNO$$$"; //URL da tenere controllato ($$$ANNO$$$ viene sostituito dall'AS corrente, ad esempio 2022-2023)
+    private static final String URL = "https://galileicrema.edu.it/novita/circolari?field_tax_anno_scolastico_target_id=All&field_numero_circolare_value=&field_oggetto_value=&field_tax_destinatari_target_id=All&page=$$$PAGE$$$"; //URL da tenere controllato ($$$PAGE$$$ verrà sostituito col numero di pagine)
     private static final String CONFIG_FILENAME = "config.ini"; //file da cui caricare la configurazione
     private static final String STATE_FILENAME = "state-v1.dat"; //file su cui salvare/caricare lo stato (per evitare di ripetere i post in caso di riavvio del bot)
 
     private static final long CHECK_NEW_INTERVAL = 600000; //controlla nuove circolari ogni 10 minuti
-    private static final long CHECK_PDFUPDATE_EVERY_INTERVALS = 36; //controlla aggiornamenti delle circolari vecchie ogni 36 intervalli (6 ore)
-    private static final int CHECK_PDFUPDATE_LAST_N = 50; //controlla solo le ultime 50 circolari per cambiamenti
+    private static final long CHECK_PDFUPDATE_EVERY_INTERVALS = 18; //controlla aggiornamenti delle circolari vecchie ogni 18 intervalli (3 ore)
+    private static final int CHECK_PAGES = 5; //controlla solo le prime 5 pagine
     private static final long DELAY_BETWEEN_POSTS = 5000; //sleep di 5 secondi tra i post per evitare il rate limiting di telegram
     private static final long DELAY_BETWEEN_RECONNECTS = 60000; //se la connessione a telegram va giù, ritenta ogni 1 minuto
     private static final long DELAY_BETWEEN_PDFHASHES = 3000; //aspetta 3 secondi tra un download di un PDF e un altro
+    private static final long DELAY_BETWEEN_LISTS = 3000; //aspetta 3 secondi tra una pagina e un'altra nell'elenco delle circolari
+    private static final long DELAY_BETWEEN_LIST_AND_PAGE = 3000; //aspetta 3 secondi tra lo scaricamento dell'elenco e l'apertura di una circolare
 
-    private static final String APP_NAME = "CircolariBot", APP_VERSION = "1.3.2";
+    private static final String APP_NAME = "CircolariBot", APP_VERSION = "1.4.0";
     private static final String USER_AGENT = APP_NAME + "/" + APP_VERSION;
 
     private static String token = null; //ID del bot fornito da botfather
     private static String channel = null; //ID del canale su cui postare le notifiche (il bot deve essere admin)
     private static TelegramBot bot = null; //collegamento a telegram
-    private static long delayAtStart=0; //quanti ms aspettare all'avvio prima di connettersi a telegram (in caso sia avviato dal sistema prima di avere una connessione funzionante)
+    private static long delayAtStart = 0; //quanti ms aspettare all'avvio prima di connettersi a telegram (in caso sia avviato dal sistema prima di avere una connessione funzionante)
 
     private static boolean testMode = false; //se attivata scrive sul terminale anzichè inviare sul canale
 
@@ -80,13 +82,14 @@ public class Main {
         }
         loadConfig();
         loadState();
-        if(delayAtStart>0){
-            System.out.println("Aspetto "+delayAtStart+"ms prima di iniziare");
+        if (delayAtStart > 0) {
+            System.out.println("Aspetto " + delayAtStart + "ms prima di iniziare");
             sleep(delayAtStart);
             System.out.println("Pronto");
         }
         for (;;) {
             long ts = System.currentTimeMillis();
+            loopCounter++;
             botLoop();
             System.gc();
             sleep(CHECK_NEW_INTERVAL - (System.currentTimeMillis() - ts));
@@ -114,36 +117,53 @@ public class Main {
     private static void botLoop() {
         ArrayList<SendMessage> postsToSend = new ArrayList<>();
         try {
-            String annoScolasticoCorrente;
-            Calendar cal = Calendar.getInstance();
-            if (cal.get(Calendar.MONTH) >= Calendar.SEPTEMBER) {
-                annoScolasticoCorrente = cal.get(Calendar.YEAR) + "-" + (cal.get(Calendar.YEAR) + 1);
-            } else {
-                annoScolasticoCorrente = (cal.get(Calendar.YEAR) - 1) + "-" + cal.get(Calendar.YEAR);
-            }
-            Document doc = Jsoup.connect(URL.replace("$$$ANNO$$$", annoScolasticoCorrente)).userAgent(USER_AGENT).get();
-            Elements circolari = doc.select("div.view-circolari-archivio-new div.view-content tr");
-            if (circolari.isEmpty()) {
-                System.err.println("Nessuna circolare estratta, forse è cambiato qualcosa nel sito?");
-            }
-            boolean checkForPdfUpdates = ++loopCounter % CHECK_PDFUPDATE_EVERY_INTERVALS == 0; //quando è true controlla gli aggiornamenti delle circolari vecchie anzichè cercare circolari nuove
-            int n = 0;
-            for (Element e : circolari) {
-                try {
-                    String numero = e.select("td.views-field-field-circolare-protocollo").first().text();
-                    String titolo = e.select("td.views-field-title > a").first().text();
-                    String descrizione = "";
+            for (int currentPage = 1; currentPage <= CHECK_PAGES; currentPage++) {
+                System.out.println(URL.replace("$$$PAGE$$$", "" + currentPage));
+                Document doc = Jsoup.connect(URL.replace("$$$PAGE$$$", "" + currentPage)).userAgent(USER_AGENT).get();
+                sleep(DELAY_BETWEEN_LISTS);
+                Elements circolari = doc.select("div.card-body");
+                if (circolari.isEmpty()) {
+                    System.err.println("Nessuna circolare estratta, forse è cambiato qualcosa nel sito?");
+                    continue;
+                }
+                boolean checkForPdfUpdates = loopCounter % CHECK_PDFUPDATE_EVERY_INTERVALS == 0; //quando è true controlla gli aggiornamenti delle circolari vecchie anzichè cercare circolari nuove
+                int n = 0;
+                for (Element e : circolari) {
                     try {
-                        descrizione = e.select("td.views-field-title > p").first().text();
-                    } catch (Throwable t) {
-                    }
-                    String link = e.select("td.views-field-title > a").first().attributes().get("href");
-                    String data = e.select("span.date-display-single").first().text();
-                    if (checkForPdfUpdates) {
-                        if (n < CHECK_PDFUPDATE_LAST_N) {
+                        String numero = "?", titolo = "", descrizione = "", link = null, data = "?";
+                        try {
+                            numero = e.select("div.head-tags span.card-tag").first().text().split("-")[1].trim();
+                            if (!numero.startsWith("n.")) {
+                                numero = "n." + numero;
+                            }
+                        } catch (Throwable t) {
+                        }
+                        try {
+                            titolo = e.select("h3.card-title").first().text();
+                        } catch (Throwable t) {
+                        }
+                        try {
+                            descrizione = e.select("div.field--name-field-breve-descrizione p").first().text();
+                        } catch (Throwable t) {
+                        }
+                        try {
+                            link = e.select("a.btn").first().attributes().get("href");
+                            if (!link.startsWith("/")) {
+                                link = "/" + link;
+                            }
+                            link = "https://galileicrema.edu.it" + link;
+                        } catch (Throwable t) {
+                        }
+                        try {
+                            data = e.select("div.head-tags span.data").first().text();
+                        } catch (Throwable t) {
+                        }
+                        if (checkForPdfUpdates) {
+                            sleep(DELAY_BETWEEN_LIST_AND_PAGE);
+                            String[] pdfUrls = getPdfUrls(link);
                             byte[] oldHash = pdfHashes.get(link);
                             if (oldHash != null) {
-                                byte[] newHash = getPdfHash(link);
+                                byte[] newHash = getPdfHashes(pdfUrls);
                                 if (newHash != null && newHash.length == oldHash.length) {
                                     boolean updated = false;
                                     for (int i = 0; i < newHash.length; i++) {
@@ -156,28 +176,38 @@ public class Main {
                                         int nUpdates = numberOfUpdates.getOrDefault(link, 0) + 1;
                                         numberOfUpdates.put(link, nUpdates);
                                         pdfHashes.put(link, newHash);
-                                        postsToSend.add(new SendMessage(channel, "Aggiornamento circolare " + numero + " del " + data + " (agg." + nUpdates + ")" + "\n" + titolo + "\n" + descrizione + "\n" + (link + "?ts=" + System.currentTimeMillis())));
+                                        String post = "Aggiornamento circolare " + numero + " del " + data + " (agg." + nUpdates + ")" + "\n" + titolo + "\n" + descrizione + "\n";
+                                        for (String pdfUrl : pdfUrls) {
+                                            post += pdfUrl + "?ts=" + System.currentTimeMillis() + "\n";
+                                        }
+                                        post = post.trim();
+                                        postsToSend.add(new SendMessage(channel, post));
                                     }
                                 }
                             }
-                            sleep(DELAY_BETWEEN_PDFHASHES);
-                        }
-                    } else {
-                        if (alreadyPosted.get(link) == null) {
-                            byte[] hash = getPdfHash(link);
-                            if (hash == null) {
-                                System.err.println("Impossibile ottenere il file della circolare");
-                            } else {
-                                postsToSend.add(new SendMessage(channel, "Circolare " + numero + " del " + data + "\n" + titolo + "\n" + descrizione + "\n" + link));
-                                alreadyPosted.put(link, System.nanoTime());
-                                pdfHashes.put(link, hash);
+                        } else {
+                            if (alreadyPosted.get(link) == null) {
+                                sleep(DELAY_BETWEEN_LIST_AND_PAGE);
+                                String[] pdfUrls = getPdfUrls(link);
+                                byte[] hash = getPdfHashes(pdfUrls);
+                                if (hash == null) {
+                                    System.err.println("Impossibile ottenere il file della circolare");
+                                } else {
+                                    String post = "Circolare " + numero + " del " + data + "\n" + titolo + "\n" + descrizione + "\n";
+                                    for (String pdfUrl : pdfUrls) {
+                                        post += pdfUrl + "?ts=" + System.currentTimeMillis() + "\n";
+                                    }
+                                    postsToSend.add(new SendMessage(channel, post));
+                                    alreadyPosted.put(link, System.nanoTime());
+                                    pdfHashes.put(link, hash);
+                                }
                             }
                         }
+                        n++;
+                    } catch (Throwable t) {
+                        System.err.println("Fallita l'estrazione delle informazioni dalla circolare. Dati grezzi: " + e);
+                        t.printStackTrace(System.err);
                     }
-                    n++;
-                } catch (Throwable t) {
-                    System.err.println("Fallita l'estrazione delle informazioni dalla circolare. Dati grezzi: " + e);
-                    t.printStackTrace(System.err);
                 }
             }
         } catch (Throwable t) {
@@ -286,41 +316,69 @@ public class Main {
         }
     }
 
-    private static final byte[] getPdfHash(String url) {
-        byte[] data = null;
-        InputStream in = null;
+    private static final String[] getPdfUrls(String link) {
+        ArrayList<String> urls = new ArrayList<>();
         try {
-            URLConnection c = new URL(url).openConnection();
-            c.setDefaultUseCaches(false);
-            c.setRequestProperty("User-Agent", USER_AGENT);
-            c.connect();
-            in = c.getInputStream();
-        } catch (Throwable ex) {
+            Document doc = Jsoup.connect(link).userAgent(USER_AGENT).get();
+            Elements elements = doc.select("h3.h5.card-title a");
+            for (Element e : elements) {
+                String url = e.attr("href");
+                if (!url.startsWith("/")) {
+                    url = "/" + url;
+                }
+                urls.add("https://galileicrema.edu.it" + url);
+            }
+            if (urls.isEmpty()) {
+                return null;
+            }
+            return urls.toArray(new String[0]);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static final byte[] getPdfHashes(String[] urls) {
+        ByteArrayOutputStream ret = new ByteArrayOutputStream();
+        for (String url : urls) {
+            byte[] data = null;
+            InputStream in = null;
+            try {
+                URLConnection c = new URL(url).openConnection();
+                c.setDefaultUseCaches(false);
+                c.setRequestProperty("User-Agent", USER_AGENT);
+                c.connect();
+                in = c.getInputStream();
+            } catch (Throwable ex) {
+                try {
+                    in.close();
+                } catch (Throwable t) {
+                }
+                return null;
+            }
+            try {
+                data = in.readAllBytes();
+            } catch (Throwable ex) {
+                try {
+                    in.close();
+                } catch (Throwable t) {
+                }
+                return null;
+            }
             try {
                 in.close();
             } catch (Throwable t) {
             }
-            return null;
-        }
-        try {
-            data = in.readAllBytes();
-        } catch (Throwable ex) {
             try {
-                in.close();
+                MessageDigest md = MessageDigest.getInstance("SHA-1");
+                ret.write(md.digest(data));
+                ret.flush();
             } catch (Throwable t) {
+                t.printStackTrace();
+                return null;
             }
-            return null;
+            sleep(DELAY_BETWEEN_PDFHASHES);
         }
-        try {
-            in.close();
-        } catch (Throwable t) {
-        }
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            return md.digest(data);
-        } catch (Throwable t) {
-            return null;
-        }
+        return ret.toByteArray();
     }
 
 }
